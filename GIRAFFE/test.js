85472fb0d6fdf2985c9469cfb757e2ba75f9085d
Tim van Mourik <timvanmourik@gmail.com>
2019-06-27 17:33:05 +0200

    Update generator file with security risks

--- GIRAFFE/test.js
+++ GIRAFFE/test.js
@@ -1,1 +1,8 @@
-module.exports = () => {alert('Hello world'); return "Hello Mars"}
+module.exports = (nodes, links) => {
+  alert('Hello world');
+  console.log(process.env);
+  console.log({nNodes: nodes.length});
+  console.log(links.length);
+
+  return "Hello Mars"
+}
