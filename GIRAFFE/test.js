cb3e9b7aa4220e7bc084fd0f4a2d0a371b4b077f
Tim van Mourik <timvanmourik@gmail.com>
2019-07-02 10:03:19 +0200

    update test module

--- GIRAFFE/test.js
+++ GIRAFFE/test.js
@@ -1,8 +1,12 @@
-module.exports = (nodes, links) => {
-  alert('Hello world');
+// module.exports = (nodes, links) => {
+//   alert('Hello world');
+//
+//   console.log({nNodes: nodes.length});
+//   console.log(links.length);
+//
+//   return {nNodes: nodes.length}
+// }
 
-  console.log({nNodes: nodes.length});
-  console.log(links.length);
-
-  return {nNodes: nodes.length}
+export function writeCode() {
+  return "Nothing to see here, move along!";
 }
